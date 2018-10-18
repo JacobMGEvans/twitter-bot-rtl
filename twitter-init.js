e7ec27895e0e82fd41098477850c2108e5e1eea7
JacobMGEvans <jacobmgevans@gmail.com>
2018-10-18 00:38:37 -0700

    CONFIG FILE

--- twitter-init.js
+++ twitter-init.js
@@ -2,5 +2,10 @@ import TwitterPackage from 'twitter';
 import {} from 'dotenv/config';
 
 const initializeConfig = {
-  consumerKey: process.env.CONSUMER_KEY
+  consumerKey: process.env.CONSUMER_KEY,
+  consumerSecret: process.env.CONSUMER_SECRET,
+  accessTokenKey: process.env.ACCESS_TOKEN_KEY,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET
 };
+
+export const Twitter = new TwitterPackage(initializeConfig);

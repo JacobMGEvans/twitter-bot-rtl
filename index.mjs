c647a4a05099d0bb32ef379a4786a3e5a63f3ca7
JacobMGEvans <jacobmgevans@gmail.com>
2018-10-24 21:39:48 -0700

    AUTH FAILURE => CODE 32

--- index.mjs
+++ index.mjs
@@ -2,30 +2,24 @@ import Twitter from './twitter-init.mjs';
 import Koa from 'koa';
 
 const app = new Koa();
-console.log('HELLO I AM TWITTER BOT');
-
-app.use(express.static('public'));
 
 /* You can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
-
-app.all('/' + process.env.BOT_ENDPOINT, function(req, res) {
+app.use(async ctx => {
+  console.log('HELLO I AM TWITTER BOT');
   /* The example below tweets out "Hello world!". */
-  Twitter.post('statuses/update', { status: 'hello world 👋' }, function(
-    err,
-    data,
-    response
-  ) {
-    if (err) {
-      console.log('error!', err);
-      res.sendStatus(500);
-    } else {
-      res.sendStatus(200);
+  ctx.body = Twitter.post(
+    'statuses/update',
+    { status: 'hello world 👋' },
+    (err, data, response) => {
+      console.log('RESPONSE', response.headers);
+      console.log('DATA', data);
+      if (err) console.log('ERROR!!!!', err);
     }
-  });
+  );
 });
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your bot is running on port ' + listener.address().port);
+const listener = app.listen(() => {
+  console.log(`Your bot is running on port ${listener.address().port}`);
 });
 
 // retweet in every 50 minutes
--- twitter-init.mjs
+++ twitter-init.mjs
@@ -1,13 +1,13 @@
-import TwitterPackage from 'twit';
+import TwitterInit from 'twit';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const initializeConfig = {
-  consumerKey: process.env.CONSUMER_KEY,
-  consumerSecret: process.env.CONSUMER_SECRET,
-  accessTokenKey: process.env.ACCESS_TOKEN_KEY,
-  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
 
-const Twitter = new TwitterPackage(initializeConfig);
+const Twitter = new TwitterInit(initializeConfig);
 export default Twitter;

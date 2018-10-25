e0c3dc25bd3a09f0fe25621462c1a3025c9fa8b2
JacobMGEvans <jacobmgevans@gmail.com>
2018-10-24 23:32:17 -0700

    AUTH FAILURE => CODE 32

--- index.mjs
+++ index.mjs
@@ -6,16 +6,24 @@ const app = new Koa();
 /* You can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 app.use(async ctx => {
   console.log('HELLO I AM TWITTER BOT');
+
+  ctx.body = Twitter.get('account/verify_credentials', { skip_status: true })
+    .catch(err => {
+      console.log('caught error', err.stack);
+    })
+    .then(result => {
+      console.log('data', result.data);
+    });
   /* The example below tweets out "Hello world!". */
-  ctx.body = Twitter.post(
-    'statuses/update',
-    { status: 'hello world 👋' },
-    (err, data, response) => {
-      console.log('RESPONSE', response.headers);
-      console.log('DATA', data);
-      if (err) console.log('ERROR!!!!', err);
-    }
-  );
+  // ctx.body = Twitter.post(
+  //   'statuses/update',
+  //   { status: 'hello world 👋' },
+  //   (err, data, response) => {
+  //     console.log('RESPONSE', response.headers);
+  //     console.log('DATA', data);
+  //     if (err) console.log('ERROR!!!!', err);
+  //   }
+  // );
 });
 
 const listener = app.listen(() => {

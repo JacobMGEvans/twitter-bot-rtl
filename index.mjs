7beb2b84ce4c10ce04fae8c89c625224c0e832a3
JacobMGEvans <jacobmgevans@gmail.com>
2018-11-14 20:43:10 -0800

    REMOVED DEAD CODE - Removed console logs and moved the intialization of Twit into the same file to avoid any modulization issues. - renamed the import of the Twit to something I have seen as a convention in examples.

--- index.mjs
+++ index.mjs
@@ -1,34 +1,46 @@
-import Twitter from './twitter-init.mjs';
+import Twit from 'twit';
 import Koa from 'koa';
+import 'babel-register';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+const initializeConfig = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+};
+
+const Twitter = new Twit(initializeConfig);
+
+// require the rest of the app that needs to be transpiled after the hook
 const app = new Koa();
-
 /* You can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 app.use(async ctx => {
   console.log('HELLO I AM TWITTER BOT');
 
-  ctx.body = Twitter.get(
-    'account/verify_credentials',
-    { skip_status: true },
-    (err, response) => {
-      console.log('DATA!!!! RESPONSESSSSSSSS', data);
-    }
-  );
-
-  /* The example below tweets out "Hello world!". */
-  // ctx.body = Twitter.post(
-  //   'statuses/update',
-  //   { status: 'hello world 👋' },
-  //   (err, data, response) => {
-  //     console.log('RESPONSE', response.headers);
-  //     console.log('DATA', data);
-  //     if (err) console.log('ERROR!!!!', err);
+  // ctx.body = Twitter.get(
+  //   'account/verify_credentials',
+  //   { skip_status: true },
+  //   (err, response) => {
+  //     console.log('DATA!!!! RESPONSESSSSSSSS', data);
   //   }
   // );
+  /* The example below tweets out "Hello world!". */
+  ctx.body = Twitter.post(
+    'statuses/update',
+    { status: 'hello world 👋' },
+    (err, data, response) => {
+      console.log('RESPONSE', response.headers);
+      console.log('DATA', data);
+      if (err) console.log('ERROR!!!!', err);
+    }
+  );
 });
 
 app.listen(3000);
-console.log(`Your bot is running on port ${listener.address(3000).port}`);
+console.log(`Your bot is running on port 3000`);
 
 // retweet in every 50 minutes
 // setInterval(retweet, 3000000)

1325eaf623bb950ad0adffbf626491c1df4bb17b
JacobMGEvans <jacobmgevans@gmail.com>
2018-11-14 23:07:40 -0800

    ADDED MIDDLEWARE FOR EXPRESS LIKE ROUTES

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
-import Twit from 'twit';
-import Koa from 'koa';
+const Twit = require('twit');
+const Koa = require('koa');
 
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 dotenv.config();
 
 const initializeConfig = {
@@ -12,32 +12,46 @@ const initializeConfig = {
 };
 
 const Twitter = new Twit(initializeConfig);
-
-// require the rest of the app that needs to be transpiled after the hook
 const app = new Koa();
+
 /* You can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 app.use(async ctx => {
   console.log('HELLO I AM TWITTER BOT');
 
-  // ctx.body = Twitter.get(
-  //   'account/verify_credentials',
-  //   { skip_status: true },
-  //   (err, response) => {
-  //     console.log('DATA!!!! RESPONSESSSSSSSS', data);
-  //   }
-  // );
-  /* The example below tweets out "Hello world!". */
-  ctx.body = Twitter.post(
+  const Post = await (ctx.body = Twitter.post(
     'statuses/update',
-    { status: 'hello world 👋' },
+    { status: 'First Tweet, Missing Hello World?' },
     (err, data, response) => {
-      console.log('RESPONSE', response.headers);
-      console.log('DATA', data);
+      console.log('*******DATA*******', data);
+      console.log('RESPONSE', response.header);
+
       if (err) console.log('ERROR!!!!', err);
     }
-  );
+  ));
+  return Post;
 });
+// ctx.body = Twitter.get(
+//   'account/verify_credentials',
+//   { skip_status: true },
+//   (err, response) => {
+//     console.log('DATA!!!! RESPONSESSSSSSSS', data);
+//   }
+// );
+/* The example below tweets out "Hello world!". */
 
+// Favorite example code
+// T.post('favorites/create', id, function(err, response){
+//   // If the favorite fails, log the error message
+//   if(err){
+//     console.log(err[0].message);
+//   }
+//   // If the favorite is successful, log the url of the tweet
+//   else{
+//     let username = response.user.screen_name;
+//     let tweetId = response.id_str;
+//     console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+//   }
+// });
 app.listen(3000);
 console.log(`Your bot is running on port 3000`);
 

cf891a4423c39be5e48b5399f4a8ee7c638c3954
JacobMGEvans <jacobmgevans@gmail.com>
2018-11-15 12:28:10 -0800

    SUCCESSFUL POST - switched to back to Koa from Express, after determining that the initialization had to occur without dotenv - Using experimental modules again with import and export default which is in a gitignored file

--- index.mjs
+++ index.mjs
@@ -1,38 +1,25 @@
-const Koa = require('koa');
-const Twit = require('twit');
-const Router = require('koa-router');
-
-const dotenv = require('dotenv');
-dotenv.config();
-
-const initializeConfig = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-};
+import Koa from 'koa';
+import Twitter from './initialization.mjs';
 
 const app = new Koa();
-const router = new Router();
-const Twitter = new Twit(initializeConfig);
+const port = 3000;
 
 /* You can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 
-router.get('/tweet', async (ctx, next) => {
-  console.log('HELLO I AM TWITTER BOT');
+console.log('HELLO I AM TWITTER BOT');
 
-  const Post = await (ctx.body = Twitter.post(
-    'statuses/update',
-    { status: 'First Tweet, Hello World?' },
-    (err, dataTweet, response) => {
-      console.log('*******####DATA####*******', dataTweet);
+app.use(
+  async ctx =>
+    (ctx.body = Twitter.post(
+      'statuses/update',
+      { status: 'Test using #Koa #server' },
+      (err, dataTweet, response) => {
+        console.log('*******####DATA####*******', dataTweet);
 
-      if (err) console.log('*****######ERROR!!!!', err);
-    }
-  ));
-
-  return Post;
-});
+        if (err) console.log('*****######ERROR!!!!', err);
+      }
+    ))
+);
 
 // Favorite example code
 // T.post('favorites/create', id, function(err, response){
@@ -47,9 +34,6 @@ router.get('/tweet', async (ctx, next) => {
 //     console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
 //   }
 // });
-app.use(router.routes()).use(router.allowedMethods());
-app.listen(3000);
-console.log(`Your bot is running on port 3000`);
-
 // retweet in every 50 minutes
 // setInterval(retweet, 3000000)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

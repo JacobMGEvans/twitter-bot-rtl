b57464ad6bc07ef7a6dc6a785f9266611c2af0cf
JacobMGEvans <jacobmgevans@gmail.com>
2018-12-02 22:06:12 -0800

    moved around the code

--- index.mjs
+++ index.mjs
@@ -6,28 +6,7 @@ import Twitter from './initialization.mjs';
 const app = new Koa();
 const router = new Router();
 const port = 3000;
-
-/* You can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
-
 console.log('HELLO I AM TWITTER BOT');
-
-router.get('/tweet', async (ctx, next) => {
-  // Might change to a stream that happens on an event like follow that sends a message to the user
-  // Once  the retweet
-  const postTweet = (ctx.body = await Twitter.post(
-    'statuses/update',
-    {
-      status: 'Bot says: Attempting to remove duplicate requests  #coding'
-    },
-    (err, dataTweet, response) => {
-      console.log('*******####DATA####*******', dataTweet);
-
-      if (err) console.log('*****######ERROR!!!!', err);
-      return response;
-    }
-  ));
-  return postTweet;
-});
 
 router.get('/search-retweet', async (ctx, next) => {
   const queryOptions = `#react OR @reactjs #javascript OR #Nodejs`;
@@ -42,8 +21,7 @@ router.get('/search-retweet', async (ctx, next) => {
       // data is an object statuses which is an array of the responses that match the q: parameter
       // data is an object with id that is needed for retweets and likes
       // text in the objecta is the actual tweet
-      data.statuses.map((element, index) => {
-        console.log(element);
+      data.statuses.map((ele, index) => {
         const { id } = data.statuses[index];
 
         foundIdSet.add(id);
@@ -74,9 +52,23 @@ router.get('/search-retweet', async (ctx, next) => {
   return [searchTweets, retweetPost];
 });
 
-//   Twitter.post('favorites/create');
-// // retweet in every 3ish  hours
-// setInterval(retweet, 10900000)
+// router.get('/tweet', async (ctx, next) => {
+//   // Might change to a stream that happens on an event like follow that sends a message to the user
+//   // Once  the retweet
+//   const postTweet = (ctx.body = await Twitter.post(
+//     'statuses/update',
+//     {
+//       status: 'Bot says: Attempting to remove duplicate requests  #coding'
+//     },
+//     (err, dataTweet, response) => {
+//       console.log('*******####DATA####*******', dataTweet);
+
+//       if (err) console.log('*****######ERROR!!!!', err);
+//       return response;
+//     }
+//   ));
+//   return postTweet;
+// });
 
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
